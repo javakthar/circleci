87c3ae66d25aecf87eb8f2b7e56b83f4bcad312c
Bruno Queiroz <bruno.queiroz@dclick.com.br>
2015-03-16 13:36:59 -0300

    ajustes no coverage

--- karma.conf.js
+++ karma.conf.js
@@ -5,13 +5,30 @@ module.exports = function(config) {
   config.set({
     autoWatch : false,
 
+    files: [
+      'src/**/*.js',
+    ],
+
     frameworks: ['jasmine'],
 
     browsers : ['PhantomJS'],
 
     plugins : [
         'karma-phantomjs-launcher',
-        'karma-jasmine'
-    ]
+        'karma-jasmine',
+        'karma-coverage'
+    ],
+
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      'src/**/!(*spec.js)': ['coverage']
+    },
+
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+
   });
 };
