f47f8978f3e9ed0852c3ae351abbab59f0c7a881
Bruno Queiroz <bruno.queiroz@dclick.com.br>
2015-03-16 10:16:54 -0300

    commit inicial

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('circleci', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngRoute', 'ngMaterial'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('circleci')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
